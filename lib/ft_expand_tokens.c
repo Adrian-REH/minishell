cb34f703200be5cc7671b34c8f3c6e2a294e3279
Maria Santillan <msantill@c2r16s5.42madrid.com>
2024-10-20 19:54:26 +0200

    flag

--- lib/ft_expand_tokens.c
+++ lib/ft_expand_tokens.c
@@ -6,7 +6,7 @@
 /*   By: msantill <msantill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 06:10:57 by adherrer          #+#    #+#             */
-/*   Updated: 2024/10/20 17:50:48 by msantill         ###   ########.fr       */
+/*   Updated: 2024/10/20 18:49:55 by msantill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,19 +34,19 @@ char	*ft_process_quote(char **env, char *line, int code)
 			arr = ft_sarradd(arr, resolve_wildcard(&result[i], &i));
 			continue ;
 		}
-		if (result[i] == '$' && result[i + 1] == '?' && flag)
+		if (result[i] == '$' && result[i + 1] == '?' && flag != 2)
 		{
 			arr = ft_sarradd(arr, ft_itoa(code));
 			i++;
 			continue ;
 		}
 		else if (result[i] == '$' \
-		&& ft_isalpha(result[i + 1]) && flag)
+		&& ft_isalpha(result[i + 1]) && flag != 2)
 		{
 			line = extract_envbyindex(result, result, env, &i);
 			arr = ft_sarradd(arr, line);
 		}
-		else if (result[i] == '\\' && result[i + 1] == 'n' && flag)
+		else if (result[i] == '\\' && result[i + 1] == 'n' && flag != 2)
 			i += (ft_sarradd(arr, "\n"), 1);
 		else
 			arr = ft_sarraddchr(arr, result[i]);
